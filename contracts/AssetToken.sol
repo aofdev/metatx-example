c34f2c75e4d8b37b4709f748819c968864ebd1d8
aofdev <aof.dev@gmail.com>
2022-09-21 12:27:03 +0700

    feat: format solidity

--- contracts/AssetToken.sol
+++ contracts/AssetToken.sol
@@ -6,23 +6,37 @@ import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AssetToken is ERC2771Context, ERC20 {
-	constructor(string memory name, string memory symbol, MinimalForwarder forwarder) 
-	ERC2771Context(address(forwarder))
-	ERC20(name, symbol) {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        MinimalForwarder forwarder
+    ) ERC2771Context(address(forwarder)) ERC20(name, symbol) {}
 
-	function _msgSender() internal view virtual override(Context, ERC2771Context) returns (address sender) {
-			return ERC2771Context._msgSender();
-	}
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(Context, ERC2771Context)
+        returns (address sender)
+    {
+        return ERC2771Context._msgSender();
+    }
 
-	function _msgData() internal view virtual override(Context, ERC2771Context) returns (bytes calldata) {
-			return ERC2771Context._msgData();
-	}
+    function _msgData()
+        internal
+        view
+        virtual
+        override(Context, ERC2771Context)
+        returns (bytes calldata)
+    {
+        return ERC2771Context._msgData();
+    }
 
-	function mint(uint256 amount) external {
-		_mint(_msgSender(), amount);
-	}
+    function mint(uint256 amount) external {
+        _mint(_msgSender(), amount);
+    }
 
-	function burn(uint256 amount) external {
-		_burn(_msgSender(), amount);
-	}
+    function burn(uint256 amount) external {
+        _burn(_msgSender(), amount);
+    }
 }
--- contracts/SimpleAssetToken.sol
+++ contracts/SimpleAssetToken.sol
@@ -1,17 +1,16 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleAssetToken is ERC20 {
-	constructor(string memory name, string memory symbol)
-	ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-	function mint(uint256 amount) external {
-		_mint(_msgSender(), amount);
-	}
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
 
-	function burn(uint256 amount) external {
-		_burn(_msgSender(), amount);
-	}
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 }
